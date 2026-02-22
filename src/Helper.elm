92539410c5776380378e44559cae039771f10f2a
Yael Aketzali <up240842@alumnos.upa.edu.mx>
2026-02-22 15:20:34 -0600

    Tarea 4 completada

--- src/Helper.elm
+++ src/Helper.elm
@@ -9,3 +9,55 @@ import Html.Attributes
 --main : Html.Html msg
 --main =
 --    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+-- EJERCICIO 1
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+
+--EJERCICIO 2
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+
+-- EJERCICIO 3
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+
+-- EJERCICIO 4
+
+
+headers : String -> Html.Html msg
+headers param =
+    Html.div
+        []
+        [ Html.h1 [] [ Html.text param ]
+        , Html.h2 [] [ Html.text param ]
+        , Html.h3 [] [ Html.text param ]
+        , Html.h4 [] [ Html.text param ]
+        , Html.h5 [] [ Html.text param ]
+        , Html.h6 [] [ Html.text param ]
+        ]
+
+
+
+--EJERCICIO 5
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink url text =
+    Html.a
+        [ Html.Attributes.href url ]
+        [ Html.text text ]
